7d8ff1d374a88036e7c7a87e3106aa8f97302b5d
tako <rina.m.takase@gmail.com>
2017-07-18 22:00:49 +0900

    Fix return value

--- lib/actions.rb
+++ lib/actions.rb
@@ -2,31 +2,31 @@ class Action
   class << self
     def cut(amount:, ingredient:)
       raise ArgumentError, "#{ingredient.class.name} is not cuttable" unless ingredient.cuttable?
-      step = "🔪 #{ingredient.title}"
+      step = "🔪\n  #{ingredient.title}"
       # loop do
       #   step += '🔪'
       #   ingredient.condition += 1
       #   break if finish_condition?(ingredient.condition, amount)
       # end
-      puts step
+      step
     end
 
     def mix(kitchenware:, ingredients: [])
       raise ArgumentError, "#{kitchenware.class.name} can't mix" unless kitchenware.actions.include?(:mix)
-      step = "🌀 #{kitchenware.title} #{ingredients.map(&:title).join}"
-      puts step
+      step = "🌀 #{kitchenware.title}\n  #{ingredients.map(&:title).join}"
+      step
     end
 
     def pickle(kitchenware:, ingredients: [])
       raise ArgumentError, "#{kitchenware.class.name} can't pickle" unless kitchenware.actions.include?(:pickle)
-      step = "(pickle) #{kitchenware.title} #{ingredients.map(&:title).join}"
-      puts step
+      step = "(pickle) #{kitchenware.title}\n  #{ingredients.map(&:title).join}"
+      step
     end
 
     def toast(kitchenware: nil, ingredients: [])
       raise ArgumentError, "#{kitchenware.class.name} can't toast" if kitchenware && !kitchenware.actions.include?(:toast)
-      step = "🍳🔥 #{ingredients.map(&:title).join}"
-      puts step
+      step = "🍳🔥\n  #{ingredients.map(&:title).join}"
+      step
     end
 
     private
