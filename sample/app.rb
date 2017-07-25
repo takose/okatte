452552b711f8baed7614514867d173f9d7725aad
tako <rina.m.takase@gmail.com>
2017-07-25 22:10:42 +0900

    rebuild syntax

--- sample/app.rb
+++ sample/app.rb
@@ -1,24 +1,4 @@
-require './lib/okatte.rb'
+require 'okatte'
 
-# potato = Potato.new(size: 'middle')
-# Action.cut(ingredient: potato, amount: 3)
-
-cooking = Cooking.new(
-  title: 'French toast',
-  ingredients: {
-    plain_bread = PlainBread.new(slice: 8) => 1,
-    egg = Egg.new => 2,
-    milk = Milk.new => 1,
-    sugar = Sugar.new => 1
-  },
-  kitchenware: [
-    bowl = Bowl.new(size: 'middle')
-  ]
-)
-
-cooking.actions << Action.cut(ingredient: plain_bread, amount: 4)
-cooking.actions << Action.mix(kitchenware: bowl, ingredients: [egg, milk, sugar])
-cooking.actions << Action.pickle(kitchenware: bowl, ingredients: [plain_bread])
-cooking.actions << Action.toast(ingredients: [plain_bread])
-
-puts cooking.start!
+# Ingredient.egg.row.mix(Kitchenware.bowl, Kitchenware.chopsticks)
+puts Ingredient.egg
