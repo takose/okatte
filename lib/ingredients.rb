cda161f4f88a05c573f4ac56a65ae20e5f824142
tako <rina.m.takase@gmail.com>
2017-07-26 15:55:34 +0900

    tmp

--- lib/ingredients.rb
+++ lib/ingredients.rb
@@ -1,92 +1,18 @@
+require 'hashie'
+require 'yaml'
+require 'pathname'
+
 class Ingredient
-  def cuttable?
-    false
+  def self.method_missing(name, *args, &block)
+    # config.send(name)
+    config.egg
   end
 
-  def raw_eatable?
-    false
-  end
-end
-
-class PlainBread < Ingredient
-  attr_accessor :slice, :title, :condition
-  def initialize(slice:)
-    @slice = slice
-    @condition = 0
-    @title = '🍞'
-  end
-
-  def cuttable?
-    true
-  end
-
-  def raw_eatable?
-    true
-  end
-end
-
-class Egg < Ingredient
-  attr_accessor :size, :condition, :title
-  def initialize
-    @condition = 0
-    @title = '🐣'
-  end
-
-  def raw_eatable?
-    true
-  end
-end
-
-class Potato < Ingredient
-  attr_accessor :size, :condition, :title
-  def initialize(size:)
-    @size = size
-    @condition = 0
-    @title = '(potato)'
-  end
-
-  def cuttable?
-    true
-  end
-
-  def raw_eatable?
-    true
-  end
-end
-
-class Sugar < Ingredient
-  attr_accessor :condition, :title
-  def initialize
-    @condition = 0
-    @title = '(sugar)'
-  end
-
-  def raw_eatable?
-    true
-  end
-end
-
-class Milk < Ingredient
-  attr_accessor :condition, :title
-  def initialize
-    @condition = 0
-    @title = '(milk)'
-  end
-
-  def raw_eatable?
-    true
-  end
-end
-
-class Chicken < Ingredient
-  attr_accessor :condition, :title
-  def initialize(part:)
-    @part = part
-    @condition = 0
-    @title = '🐔#{part}'
-  end
-
-  def cuttable?
-    true
+  def self.config
+    unless @config
+      config_file = "#{File.dirname(__FILE__)}/../config/ingredients.yml"
+      @config = Hashie::Mash.load(config_file)
+    end
+    @config
   end
 end
